8c2d0489de79b077ef321cbf4fa6d30bec2feed4
MAC <olorungbohunsamuel@gmail.com>
2022-10-10 13:29:01 +0100

    FrontEnd Version 2 and added Kubernetes Object

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ae48b710bb2b3493baa5e0a2a4f9b91b-131817020.us-east-1.elb.amazonaws.com'
 };
 
 /*
